4d866b8f755408b03f7c723206cf9f8eddb1a0e3
Sevgi <sevgi2308@gmail.com>
2022-09-07 17:16:30 +0300

    add new paste is now functional

--- src/components/AddNewPaste.tsx
+++ src/components/AddNewPaste.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
-export function AddNewPaste():JSX.Element{
-    const [titleInput, setTitleInput]= useState<string>("");
-    const [bodyInput, setBodyInput]= useState<string>("");
-    return(
-        <>
-            <input type="text" placeholder="title (optional)" value={titleInput}></input>
-            <input type="text" placeholder="type paste here..." value={bodyInput}></input>
-            <button>Submit</button>
-        </>
-    )
-
+export function AddNewPaste(): JSX.Element {
+  const [titleInput, setTitleInput] = useState<string>("");
+  const [bodyInput, setBodyInput] = useState<string>("");
+  function submitPaste() {
+    axios
+      .post(`https://mae-sevgi-pastebin.herokuapp.com/pastes`, {
+        pastebody: bodyInput,
+        title: titleInput,
+      })
+      .then(() => setBodyInput(""))
+      .then(() => setTitleInput(""));
+  }
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="title (optional)"
+        value={titleInput}
+        onChange={(e) => setTitleInput(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="type paste here..."
+        value={bodyInput}
+        onChange={(e) => setBodyInput(e.target.value)}
+      ></input>
+      <button onClick={() => submitPaste()}>Submit</button>
+    </>
+  );
 }
